0fb6f7eb91e64bf74b3161b6ef49a91286da40e5
JJangGyu <pridom112@naver.com>
2022-07-23 18:44:42 +0900

    compare react-refactoring extends

--- compare-react/react-refactorting/src/App.js
+++ compare-react/react-refactorting/src/App.js
@@ -4,6 +4,8 @@ import SearchForm from './components/SearchForm.js';
 import SearchResult from './components/SearchResult.js';
 import store from './Store.js';
 import Tabs, {TabType} from './components/Tabs.js';
+import KeywordList from './components/KeywordList.js';
+import HistoryList from './components/HistoryList.js';
 
 export default class App extends React.Component {
 
@@ -28,6 +30,7 @@ export default class App extends React.Component {
   search(searchKeyword){
     const searchResult = store.search(searchKeyword);
     this.setState({
+      searchKeyword,
       searchResult,
       submitted : true,
     });
@@ -62,8 +65,8 @@ export default class App extends React.Component {
                   selectedTab={selectedTab} 
                   onChange={(selectedTab) => this.setState({ selectedTab })}
                 />
-                {selectedTab === TabType.KEYWORD && <>TODO: 추천검색어 목록 </>}
-                {selectedTab === TabType.HISTORY && <>TODO: 최근검색어 목록 </>}
+                {selectedTab === TabType.KEYWORD && <KeywordList onClick={keyword => this.search(keyword)} />}
+                {selectedTab === TabType.HISTORY && <HistoryList onClick={keyword => this.search(keyword)} />}
               </>
               )
             }
